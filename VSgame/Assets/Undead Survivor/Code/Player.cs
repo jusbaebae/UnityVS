256cc121b86994cf8f41837ffe42b41ae635a5d6
jusbaebae <118880712+jusbaebae@users.noreply.github.com>
2023-09-17 00:10:08 +0900

    first commit

---
+++ VSgame/Assets/Undead Survivor/Code/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+}
